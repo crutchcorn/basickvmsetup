f8bfad541bc8ef394ba02247b472857f3f60d81d
Corbin Crutchley <crutchcorn@gmail.com>
2015-08-28 05:09:39 -0700

    Update kvminstall.sh

--- kvminstall.sh
+++ kvminstall.sh
@@ -7,6 +7,6 @@ if [ `egrep -c '(vmx|svm)' /proc/cpuinfo` = 0 ]; then
 	echo "Sorry, but you have incompatable hardware"
 	exit
 fi
-apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils virt-manager
+apt-get install -y qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils virt-manager
 adduser `id -un` libvirtd
 echo "Please log out and log back in. Then, you'll have KVM set up!"
